7edf4b0ab328d401a7d7f93b19dfa57c95ddb475
benjp009 <patin.benjamin@gmail.com>
2021-12-14 10:20:53 +0100

    fix linters

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import _ from 'lodash';
 import './style.css';
+
+function component() {
+  const element = document.createElement('div');
+  return element;
+}
+
+document.body.appendChild(component());
