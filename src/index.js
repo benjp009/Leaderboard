a1be98d04162dcbda87ac254d93f627789c6d3ea
benjp009 <patin.benjamin@gmail.com>
2021-12-14 10:17:49 +0100

    Remove unnecessary function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
 import _ from 'lodash';
 import './style.css';
-
-
-function component() {
-  const element = document.createElement('div');
-  // eslint-disable-next-line no-unused-vars
-  return element;
-}
-
-document.body.appendChild(component());
